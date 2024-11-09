94947441afe42c2d33189697b95fd76d5486d01e
Petar-I-Ivanov <p.ivanov2955@gmail.com>
2024-11-09 13:37:53 +0200

    Added user model

--- src/main/java/pu/fmi/rent_a_car/user/model/User.java
+++ src/main/java/pu/fmi/rent_a_car/user/model/User.java
@@ -1,4 +1,4 @@
-package pu.fmi.rent_a_car.office.model;
+package pu.fmi.rent_a_car.user.model;
 
 import static lombok.AccessLevel.PRIVATE;
 
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -16,16 +17,25 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = PRIVATE)
-public class Office {
+public class User {
 
   @Id @GeneratedValue Long id;
 
-  @Column(length = 50, nullable = false)
-  String country;
+  @Column(length = 60, nullable = false)
+  String firstName;
 
-  @Column(length = 50, nullable = false)
-  String city;
+  @Column(length = 60, nullable = false)
+  String lastName;
 
   @Column(length = 255, nullable = false)
   String address;
+
+  @Column(length = 30, nullable = false, unique = true)
+  String phone;
+
+  @Column(nullable = false)
+  Short age;
+
+  @Column(nullable = false)
+  boolean hasCarAccidents;
 }
---
+++ src/main/resources/db/migration/V0.0.004__Create_user_table_insert_values.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `user` (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(60) NOT NULL,
+    lastName VARCHAR(60) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    phone VARCHAR(30) NOT NULL UNIQUE,
+    age SMALLINT NOT NULL,
+    hasCarAccidents BOOLEAN NOT NULL
+);
+
+INSERT INTO `user` VALUES 
+(NEXT VALUE FOR hibernate_sequence, 'Petar', 'Ivanov', 'Sliven', '0888888888', 23, false),
+(NEXT VALUE FOR hibernate_sequence, 'Georgi', 'Georgiev', 'Plovdiv', '0877777777', 20, true),
+(NEXT VALUE FOR hibernate_sequence, 'Ivan', 'Ivanov', 'Sofia', '0866666666', 30, false);
